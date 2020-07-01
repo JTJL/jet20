875804ab9ad62599ea541b10630d0e83fec9555d
yudongwu-watermirror <yudongwu@watermirror.ai>
2020-07-01 10:36:23 +0800

    完善gitignore；增加pipfile

--- jet20/frontend/__init__.py
+++ jet20/frontend/__init__.py
@@ -2,6 +2,8 @@ from jet20.frontend.variable import Variable
 from jet20.frontend.expression import Expression
 from jet20.frontend.problem import Problem
 
+__author__ = """Dongwu Yu"""
+__email__ = "yudongwu@watermirror.ai"
 __version__ = '0.0.1'
 
 __all__ = [
