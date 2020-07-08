3532d395a3bcaa21125a86a3851451de1c93059f
ioriiod0 <ioriiod0@gmail.com>
2020-07-08 12:17:30 +0800

    update version..

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ioriiod0/jet20',
-    version='0.5.1',
+    version='0.5.2',
     zip_safe=False,
 )
