a730434742747483acc93f50499c0d89f9ab337a
ioriiod0 <ioriiod0@gmail.com>
2020-07-13 14:21:12 +0800

    update author and email

--- jet20/__init__.py
+++ jet20/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Jet20."""
 
-__author__ = """ioriiod0"""
-__email__ = 'ioriiod0@gmail.com'
+__author__ = """watermirror"""
+__email__ = 'gaolei@watermirror.ai'
 __version__ = '0.5.3'
 
 from jet20.frontend import *
